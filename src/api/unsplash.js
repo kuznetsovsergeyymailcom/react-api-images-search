e30efb897e210567fd09c5cd3fbc6dd548149c1c
kuznetsovsergeyymailcom <kuznetsovsergey@ymail.com>
2020-06-29 20:15:25 +0300

    Update unsplash.js

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export default axios.create({
     baseURL: 'https://api.unsplash.com',
     headers: {
-        Authorization: 'Client-ID tnVq053eLKUjgh5YazXIb2LkT5QDEy1h7uyPiaKqm8c'
+        Authorization: 'Client-ID ***'
     }
 
 });
